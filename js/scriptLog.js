e11dfe05eeb9040b66d109105ce76cab06252b92
Souha <souhachriki@gmail.com>
2024-12-07 10:29:50 +0100

    Initial commit with website files

---
+++ js/scriptLog.js
@@ -0,0 +1,21 @@
+//welcome message + gif
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent form submission
+    const username = document.getElementById("exampleInputUsername").value;
+    const welcomeMessageDiv = document.getElementById("welcomeMessage");
+    const userWelcome = document.getElementById("userWelcome");
+    const gifContainer = document.getElementById("gifContainer");
+
+    userWelcome.textContent = `Welcome back, ${username}!`;
+    const gifUrl = "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExMmhuNXhlbnc5NnJwNTM2eGV0anJsajRkano3NHVqeDl0dmQ3ZnV2cyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Jnf482UXAJ4E1Ojcf2/giphy.webp"; // Example GIF URL
+    gifContainer.src = gifUrl;
+
+    welcomeMessageDiv.style.display = "block";
+    
+    // Redirect to the blog section after 2 seconds
+    setTimeout(() => {
+        window.location.href = "index.html#Blog";
+    }, 3000);
+});
+
+
---
+++ js/scriptSign.js
@@ -0,0 +1,42 @@
+document.getElementById("loginForm").addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent form submission
+
+    
+    const username = document.getElementById("exampleInputUsername").value;
+    const dobInput = document.getElementById("exampleInputDate").value;
+
+    // Age validation (must be +13)
+    const dob = new Date(dobInput);
+    const today = new Date();
+    const age = today.getFullYear() - dob.getFullYear();
+    const monthDiff = today.getMonth() - dob.getMonth();
+    const dayDiff = today.getDate() - dob.getDate();
+
+    if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)) {
+        age--;
+    }
+
+    if (age < 13) {
+        alert("You must be at least 13 years old to sign up.");
+        return; // Exit if age validation fails
+    }
+
+    // welcome message + gif
+    const welcomeMessageDiv = document.getElementById("welcomeMessage");
+    const userWelcome = document.getElementById("userWelcome");
+    const gifContainer = document.getElementById("gifContainer");
+
+    userWelcome.textContent = `Welcome to the community, ${username}!`;
+    const gifUrl = "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExanYwNWw1dWxmdmY3NjNvZXR4eDRiNm12NWxmODdpbWxxYnZzZXI4biZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/DPH2d2WQsvTVEDVa2p/giphy.webp"; // Example GIF URL
+    gifContainer.src = gifUrl;
+
+    welcomeMessageDiv.style.display = "block"; // Show the welcome message
+
+    // Redirect to the blog section after 3 seconds
+    setTimeout(() => {
+        window.location.href = "index.html#Blog";
+    }, 3000);
+    
+    
+});
+
